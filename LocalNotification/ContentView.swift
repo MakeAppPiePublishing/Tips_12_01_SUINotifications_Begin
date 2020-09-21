3053d6d3823ec3a7d0a2c71bf1991ab0bc42169d
Steve Lipton <steve@makeapppie.com>
2020-09-21 12:11:23 -0500

    Update ContentView.swift
    Missing comments added.

--- LocalNotification/ContentView.swift
+++ LocalNotification/ContentView.swift
@@ -4,6 +4,14 @@
 //
 //  Created by Steven Lipton on 8/14/20.
 //
+// An exercise file for iOS Development Tips Weekly
+// A weekly course on LinkedIn Learning for iOS developers
+//  Season 12 (Q4 2020) video 01
+//  by Steven Lipton (C)2020, All rights reserved
+// Learn more at https://linkedin-learning.pxf.io/YxZgj
+//This Week:  Learn how to implement local notifications in SwiftUI
+//  For more code, go to http://bit.ly/AppPieGithub
+
 
 import SwiftUI
 
